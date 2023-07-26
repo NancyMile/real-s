ffa27663fce75e15e7e9c5b907c4a777b6d3886a
nancy <smithnancy@outlook.com>
2023-07-26 15:14:18 +1000

    header removed  from blog and contact us

--- blog.php
+++ blog.php
@@ -1,28 +1,6 @@
 <?php
   include './includes/templates/header.php';
 ?>
-  <body>
-    <header class="header">
-      <div class="contenedor header-content">
-        <div class="bar">
-          <a href="/">
-            <img src="build/img/logo.svg" alt="logo" />
-          </a>
-          <div class="menu-mobile">
-            <img src="build/img/barras.svg" alt="menu">
-          </div>
-          <div class="right">
-            <img class="dark-mode-button" src="build/img/dark-mode.svg">
-            <nav class="navigation">
-              <a href="about.php">About</a>
-              <a href="advert.php">Adverts</a>
-              <a href="blog.php">Blog</a>
-              <a href="contact.php">Contact Us</a>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </header>
     <main class="contenedor section contenido-centrado">
       <h1>Our Blog</h1>
       <article class="entry-blog">
