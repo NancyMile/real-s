ef538f303384d67baade20560bb2db8cd379f3b4
nancy <smithnancy@outlook.com>
2023-07-27 11:33:55 +1000

    index file  table for display list of properties created and styles applied

--- admin/index.php
+++ admin/index.php
@@ -10,5 +10,31 @@
       <p class="alert success">Add Created!</p>
     <?php endif; ?>
     <a href="/admin/properties/create.php" class="btn btn-green">Create</a>
+
+    <table class="properties">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Image</th>
+          <th>Price</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td><img src="/images/" alt="image" class="image-table"></td>
+          <td></td>
+          <td>
+            <a href="#" class="btn-yellow-block">Update</a>
+            <a href="#" class="btn-red-block">Delete</a>
+          </td>
+        </tr>
+      </tbody>
+
+    </table>
+
 </main>
 <?php addTemplate('footer'); ?>
