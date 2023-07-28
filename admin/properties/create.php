0a18bcbcfb19fcf50f79e9a5d810306077dbf9f1
nancy <smithnancy@outlook.com>
2023-07-28 19:08:31 +1000

    new template form_properties created, form removed  from create file , now including the template

--- admin/properties/create.php
+++ admin/properties/create.php
@@ -11,14 +11,6 @@
   $query = "SELECT * FROM sellers";
   $result = mysqli_query($db,$query);
 
-  $title = '';
-  $price = '';
-  $description = '';
-  $rooms = '';
-  $bathrooms = '';
-  $garages = '';
-  $sellerId = '';
-
   //executes after the user sends the form
   if($_SERVER['REQUEST_METHOD'] === 'POST'){
     //create a new instance
@@ -71,37 +63,8 @@
     <?php endif; ?>
 
     <form class="formulario" method="POST" action="/admin/properties/create.php" enctype="multipart/form-data">
-      <fieldset>
-        <legend>Genral Info</legend>
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title" placeholder="title" value="<?php echo $title; ?>">
-        <label for="price">Price</label>
-        <input type="number" name="price" id="price" placeholder="price" value="<?php echo $price; ?>">
-        <label for="title">Image</label>
-        <input type="file" id="image" accept="image/jpeg,image/png" name="image">
-        <label for="description">Description</label>
-        <textarea id="description" name="description"><?php echo $description; ?></textarea>
-      </fieldset>
-      <fieldset>
-        <legend>Property Info</legend>
-        <label for="rooms">Rooms</label>
-        <input type="number" id="rooms" name="rooms" min="1" max="9" placeholder="Eg: 3" value="<?php echo $rooms; ?>">
-        <label for="bathrooms">Bathrooms</label>
-        <input type="number" id="bathrooms" name="bathrooms" min="1" max="4" placeholder="Eg: 3" value="<?php echo $bathrooms; ?>">
-        <label for="garages">Garages</label>
-        <input type="number" id="garages" name="garages" min="1" max="3" placeholder="Eg: 3" value="<?php echo $garages; ?>">
-      </fieldset>
-      <fieldset>
-        <legend>Seller Info</legend>
-        <label for="name">Name</label>
-        <select name="seller_id">
-          <option value="" selected disabled>-- Select --</option>
-            <?php while($seller = mysqli_fetch_assoc($result)) : ?>
-              <option <?php echo $seller['id'] === $sellerId ? 'selected': ''; ?> value="<?php echo $seller['id']?>"><?php echo $seller['name'].' '.$seller['lastname'] ?></option>
-            <?php endwhile; ?>
-        </select>
-      </fieldset>
-      <input type="submit" value="Save" class="btn btn-green">
+    <?php include '../../includes/templates/form_properties.php'; ?>
+    <input type="submit" value="Save" class="btn btn-green">
     </form>
 </main>
 <?php addTemplate('footer'); ?>
---
+++ includes/templates/form_properties.php
@@ -0,0 +1,30 @@
+<fieldset>
+    <legend>Genral Info</legend>
+    <label for="title">Title</label>
+    <input type="text" name="title" id="title" placeholder="title" value="<?php echo sanitizingHtml($property->title); ?>">
+    <label for="price">Price</label>
+    <input type="number" name="price" id="price" placeholder="price" value="<?php echo sanitizingHtml($property->price); ?>">
+    <label for="title">Image</label>
+    <input type="file" id="image" accept="image/jpeg,image/png" name="image">
+    <label for="description">Description</label>
+    <textarea id="description" name="description"><?php echo sanitizingHtml($property->description); ?></textarea>
+</fieldset>
+<fieldset>
+    <legend>Property Info</legend>
+    <label for="rooms">Rooms</label>
+    <input type="number" id="rooms" name="rooms" min="1" max="9" placeholder="Eg: 3" value="<?php echo sanitizingHtml($property->rooms); ?>">
+    <label for="bathrooms">Bathrooms</label>
+    <input type="number" id="bathrooms" name="bathrooms" min="1" max="4" placeholder="Eg: 3" value="<?php echo sanitizingHtml($property->bathrooms); ?>">
+    <label for="garages">Garages</label>
+    <input type="number" id="garages" name="garages" min="1" max="3" placeholder="Eg: 3" value="<?php echo sanitizingHtml($property->garages); ?>">
+</fieldset>
+<fieldset>
+    <legend>Seller Info</legend>
+    <label for="name">Name</label>
+    <select name="seller_id">
+        <option value="" selected disabled>-- Select --</option>
+        <?php while($seller = mysqli_fetch_assoc($result)) : ?>
+            <option <?php echo $seller['id'] === $sellerId ? 'selected': ''; ?> value="<?php echo $seller['id']?>"><?php echo $seller['name'].' '.$seller['lastname'] ?></option>
+        <?php endwhile; ?>
+    </select>
+</fieldset>
