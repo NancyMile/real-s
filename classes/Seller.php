778a5a8e92ff4ff9e845b73ebe3ba63cf0ff6d30
nancy <smithnancy@outlook.com>
2023-07-31 11:06:33 +1000

    seller class validation method added

--- classes/Seller.php
+++ classes/Seller.php
@@ -17,4 +17,24 @@ class Seller extends ActiveRecord {
         $this->lastname = $args['lastname'] ?? '';
         $this->phone = $args['phone'] ?? '';
     }
+
+    public function validate(){
+        if(!$this->name){
+            self::$errors[] = 'Please enter name';
+        }
+
+        if(!$this->lastname){
+            self::$errors[] = 'Please enter lastname';
+        }
+
+        if(!$this->phone){
+            self::$errors[] = 'Please enter phone';
+        }
+
+        if(!preg_match('/[0-9]{10}/',$this->phone)){
+            self::$errors[] = 'Format is not valid';
+        }
+
+        return self::$errors;
+    }
 }
